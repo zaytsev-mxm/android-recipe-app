768e267cb67eb6ae1e5f876130119de613810c47
Max Zaytsev <max.zaytsev@team.bumble.com>
2024-12-29 14:41:35 +0000

    IDEA settings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "dev.maxiscoding.myrecipeapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "dev.maxiscoding.myrecipeapp"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
