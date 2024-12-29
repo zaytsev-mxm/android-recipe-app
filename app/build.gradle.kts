ad4be1dc106e385814a5b016cd34507b8a44f42d
Max Zaytsev <max.zaytsev@team.bumble.com>
2024-12-29 14:53:56 +0000

    Dependencies added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,9 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter.gson)
+    implementation(libs.coil.compose)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
