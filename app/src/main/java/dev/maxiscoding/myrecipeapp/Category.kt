e9fb6628a4fbd893be0e418b481b3e8ed93d4f28
Max Zaytsev <max.zaytsev@team.bumble.com>
2025-01-06 20:03:21 +0000

    Make the Category pacelable

--- app/src/main/java/dev/maxiscoding/myrecipeapp/Category.kt
+++ app/src/main/java/dev/maxiscoding/myrecipeapp/Category.kt
@@ -1,10 +1,14 @@
 package dev.maxiscoding.myrecipeapp
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Category(
     val idCategory: String,
     val strCategory: String,
     val strCategoryThumb: String,
     val strCategoryDescription: String
-)
+): Parcelable
 
 data class CategoriesResponse(val categories: List<Category> = listOf())
